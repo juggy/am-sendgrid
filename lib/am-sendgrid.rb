a351229131c67f79eaad6765352f4125521cff46
Julien Guimont <julien@porkepic.com>
2011-04-13 17:31:48 -0400

    Rails 3

--- lib/am-sendgrid.rb
+++ lib/am-sendgrid.rb
@@ -8,12 +8,12 @@ module ActionMailer
       @sendgrid_header ||= SmtpApiHeader.new
     end
 
-    def create_mail_with_sendgrid_header
+    def mail_with_sendgrid_header
       headers["X-SMTPAPI"] = sendgrid_header.asJSON if sendgrid_header
-      create_mail_without_sendgrid_header
+      mail_without_sendgrid_header
     end
 
-    alias_method_chain :create_mail, :sendgrid_header
+    alias_method_chain :mail, :sendgrid_header
 
     def sendgrid_filter_setting(*args)
       sendgrid_header.addFilterSetting *args
